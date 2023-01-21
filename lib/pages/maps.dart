40d20881de280fc971c2925b1e576fd5304a6f08
Stanley <kim4111@purdue.edu>
2023-01-20 20:17:47 -0500

    google maps update

--- lib/pages/maps.dart
+++ lib/pages/maps.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsPage extends StatefulWidget {
   const MapsPage({super.key});
@@ -10,16 +11,17 @@ class MapsPage extends StatefulWidget {
 }
 
 class MapsPageState extends State<MapsPage> {
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(37.773972, -122.431297),
+    zoom: 11.5,
+  );
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "This is the Maps Page!!!",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 40.0,
-        ),
-      ),
-    );
+    return Scaffold(
+        body: GoogleMap(
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+      initialCameraPosition: _initialCameraPosition,
+    ));
   }
 }
