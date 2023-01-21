1fbbf69518d65d021173e942431b184d8144a04a
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-01-20 23:51:26 -0500

    Visible Map

--- lib/pages/maps.dart
+++ lib/pages/maps.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapsPage extends StatefulWidget {
   const MapsPage({super.key});
@@ -10,6 +12,8 @@ class MapsPage extends StatefulWidget {
 }
 
 class MapsPageState extends State<MapsPage> {
+  final LatLng _center = LatLng(40.4285, 86.9220);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +22,24 @@ class MapsPageState extends State<MapsPage> {
           title: const Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: Container(),
+        body: FlutterMap(
+          options: MapOptions(
+            center: _center,
+            zoom: 9.2,
+          ),
+          nonRotatedChildren: [
+            AttributionWidget.defaultWidget(
+              source: 'OpenStreetMap contributors',
+              onSourceTapped: null,
+            ),
+          ],
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+            ),
+          ],
+        ),
       ),
     );
   }
