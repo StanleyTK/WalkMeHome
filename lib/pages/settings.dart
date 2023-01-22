410de21585f097e289b03f66fa04dfdcc899b554
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-01-22 05:15:52 -0500

    Updated Settings UI

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -29,16 +29,22 @@ class SettingsPageState extends State<SettingsPage> {
               tiles: <SettingsTile>[
                 SettingsTile.navigation(
                   leading: const Icon(Icons.person),
-                  title: const Text('Stanley'),
+                  title: const Text('Account Settings'),
                   value: const Text('stanleykim2003'),
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(Icons.mobile_friendly),
-                  title: const Text('Add Friends'),
+                  leading: const Icon(Icons.travel_explore),
+                  title: const Text('Past Trips'),
                 ),
                 SettingsTile.navigation(
-                  leading: const Icon(Icons.remove),
-                  title: const Text('Remove Friends'),
+                  leading: const Icon(Icons.language),
+                  title: const Text('Language'),
+                  value: const Text('English'),
+                ),
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.light_mode),
+                  title: const Text('Theme'),
+                  value: const Text('Light Mode'),
                 ),
                 SettingsTile.navigation(
                   leading: const Icon(Icons.logout),
