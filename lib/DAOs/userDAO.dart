29057c195eec1c3f4c952075b2a7007352db2adf
Stanley <kim4111@purdue.edu>
2023-01-22 01:02:03 -0500

    User Data

--- lib/DAOs/userDAO.dart
+++ lib/DAOs/userDAO.dart
@@ -1,1 +1,16 @@
-class userDAO {}
+import 'package:walkmehome/models/user.dart';
+
+class UserDAO {
+  User user =
+      User('Stanley', 'stanleykim2003', 'stanleykim2003@gmail.com', [], Null);
+
+  List<User> friends = [
+    User('Ariq', 'LukaMagic66', 'LukaMagic66@yahoo.com', [], Null),
+    User('Joshua Adith George', 'd2thefraud77', 'd2thefraud77@outlook.com', [],
+        Null),
+    User('Daniyal', 'adgasdasdfvcx', 'adgasdasdfvcx@gmail.com', [], Null),
+    User('Daniel', 'LeagueGrinder', 'LeagueGrinder@gmail.com', [], Null),
+  ];
+}
+
+class UserTileBuilder {}
