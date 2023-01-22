279a4380eccf5b517af6e3bdae774b0582497bd6
danielxu <xu1708@purdue.edu>
2023-01-22 02:41:03 -0500

    Location pop layout

--- lib/pages/locationpopup.dart
+++ lib/pages/locationpopup.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:walkmehome/pages/navbar.dart';
 
+class PopPage extends StatefulWidget {
+  const PopPage({super.key});
+
+  @override
+  State<PopPage> createState() => PopPageState();
+}
+
+class PopPageState extends State<PopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange[400],
+      appBar: AppBar(
+        title: Text('D E S T I N A T I O N  I N F O'),
+      ),
+      body: Padding(
+          //Put map here
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  color: Colors.deepOrange[400],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 8,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        color: Colors.deepOrange[300],
+                        height: 120,
+                      ),
+                    );
+                  }),
+            ),
+          ])),
+    );
+  }
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -93,7 +93,7 @@ class LoginPageState extends State<LoginPage> {
                   child: Column(
                     children: <Widget>[
                       const SizedBox(
-                        height: 60,
+                        height: 40,
                       ),
                       Container(
                         decoration: BoxDecoration(
@@ -165,7 +165,7 @@ class LoginPageState extends State<LoginPage> {
                         ),
                       ),
                       const SizedBox(
-                        height: 40,
+                        height: 50,
                       ),
                       const Text(
                         "Continue with social media",
